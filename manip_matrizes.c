2a17f6a36127c1ab0e4693fd4fecd099616bf88f
Fernando Bordin <fernandobordin1234@gmail.com>
2024-10-22 20:17:05 -0300

    Esqueleto das funções de frações
    Criação das função de frações, funções que usaremos com bastante frequência mais pra frente.

--- manip_matrizes.c
+++ manip_matrizes.c
@@ -3,3 +3,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+//------- FUNÇÕES DAS FRAÇÕES ---------
+Fracao simplificar(Fracao f) {
+  
+  return f;
+}
+
+Fracao somar(Fracao a, Fracao b) {
+
+  return simplificar(resultado);
+}
+
+Fracao subtrair(Fracao a, Fracao b) {
+
+  return simplificar(resultado);
+}
+
+Fracao dividir(Fracao a, Fracao b) {
+  
+  return simplificar(resultado);
+}
+
+Fracao multiplicar(Fracao a, Fracao b) {
+
+  return simplificar(resultado);
+}
+
+Fracao lerFracao() {
+  
+  return simplificar(f);
+}
+
+Fracao fracPadrao() {
+
+  return simplificar(f);
+}
+
+Fracao frac_unitaria_Positiva() {
+
+  return simplificar(f);
+}
+
+Fracao frac_unitaria_Negativa() {
+
+  return simplificar(f);
+}
